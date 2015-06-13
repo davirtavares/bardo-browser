f5137c6608da859d1a53b99acb0d407da8fa843c
Davi Tavares <davirtavares@gmail.com>
2015-06-13 16:26:30 -0300

    Just some identation fixes.

--- plugin/chrome/js/content-script.js
+++ plugin/chrome/js/content-script.js
@@ -9,19 +9,23 @@
  * - a extensão então gera o código da página e junta com os elementos
  * extraídos, e enviar para o webservice persistir;
  */
-window.addEventListener("mousemove", function(event) {
-    var element = document.elementFromPoint(event.clientX, event.clientY);
 
-    if (element) {
-    }
-}, true);
+(function() {
+    window.addEventListener("mousemove", function(event) {
+        var element = document.elementFromPoint(event.clientX, event.clientY);
 
-window.addEventListener("click", function(event) {
-    event.stopPropagation();
+        if (element) {
+        }
+    }, true);
 
-    var element = event.target;
+    window.addEventListener("click", function(event) {
+        event.preventDefault();
+        event.stopPropagation();
 
-    if (element) {
-        element.setAttribute(prefix + "-selected", "");
-    }
-}, true);
+        var element = event.target;
+
+        if (element) {
+            element.setAttribute(prefix + "-selected", "");
+        }
+    }, true);
+})();
